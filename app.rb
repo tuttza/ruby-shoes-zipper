881b7a3f53741027006241940e195e241a255468
Zach Tuttle <zachary.tuttle@crossoverhealth.com>
2018-12-14 22:54:58 -0800

    fixing up some logic

--- app.rb
+++ app.rb
@@ -1,16 +1,17 @@
 class ShoesToolKit
   def self.calculate_list_box_width(str_array)
-    (str_array.map { |el| (el.length + el.length) / (str_array.size) }.inject(0) { |sum, x| sum + x }) * 10
+    (str_array.map { |el| (el.length + el.length) / (str_array.size) }.inject(0) do |sum, x| sum + x  end) * 10
   end
 
   def self.str_value_exist?(value)
-    (value != nil || value != "" || value != " ")
+    debug("value: #{value.inspect}")
+    (value == nil || value == "" || value == " ")
   end
 end
 
 class Zipper
   def self.zip(dir, zip_dir, remove_after = false)
-    Zip::ZipFile.open(zip_dir, ::Zip::ZipFile::CREATE)do |zipfile|
+    Zip::ZipFile.open(zip_dir, Zip::ZipFile::CREATE)do |zipfile|
       Find.find(dir) do |path|
         Find.prune if File.basename(path)[0] == ?.
         dest = /#{dir}\/(\w.*)/.match(path)
@@ -47,7 +48,7 @@ class Zipper
 end
 
 Shoes.setup do
-  gem "zip"
+  gem 'zip'
   require("fileutils")
   require("find")
   require("date")
@@ -61,21 +62,23 @@ Shoes.app(width: 350, height: 250, title: "Ruby Zipper", resizable: false) do
   flow left: 65, top: 20 do 
     @file_path_str = edit_line(width: 250, right: 154)
 
-    button("...", width: 75, right: 75) do 
+    button("...", width: 60, right: 85) do 
       @selected_dir = ask_open_folder
       @file_path_str.text = @selected_dir
 
-      @dir_contents = Dir.entries(@selected_dir)
-      @dir_contents_size = @dir_contents.size
-      @zip_file_name = "#{File.basename(@selected_dir)}_#{Date.today.strftime("%Y%m%d").to_s}.zip"
-      
-      debug(@dir_contents)
-      debug("Files Found: #{@dir_contents_size}")
+      if @selected_dir 
+        @dir_contents = Dir.entries(@selected_dir)
+        @dir_contents_size = @dir_contents.size
+        @zip_file_name = "#{File.basename(@selected_dir)}_#{Date.today.strftime("%Y%m%d").to_s}.zip"
+        
+        debug(@dir_contents)
+        debug("Files Found: #{@dir_contents_size}")
+      end
     end
 
     flow do 
       @operation = OPERATIONS.first
-      
+
       lb_width = ShoesToolKit.calculate_list_box_width(OPERATIONS)
 
       para "Operation: "
@@ -89,21 +92,23 @@ Shoes.app(width: 350, height: 250, title: "Ruby Zipper", resizable: false) do
     stack right: 54, top: 80 do 
       @zip_progress = progress(width: 330)
 
-      button("Start", width: 95) do 
-        unless ShoesToolKit.str_value_exist?(@selected_dir)
-          alert("You must select a directory or file to zip before starting.", title: "Could not start operation:")
-        else
-          case @operation
-          when "Zip"
-            Thread.new { Zipper.zip(File.join(@selected_dir), File.join(ENV["HOME"], "Desktop/#{@zip_file_name}")) }.join
-
-            animate do |i|
-              @zip_progress.fraction = (@dir_contents_size % Zipper.progress ) * 100
-            end
-          when "Unzip"
-            alert("unzip operation selected!")
+      flow do 
+        button("Start", width: 95, left: 112) do 
+          if ShoesToolKit.str_value_exist?(@selected_dir)
+            alert("You must select a directory or file to zip before starting.", title: "Could not start operation:")
           else
-            alert("you must select an operation!")
+            case @operation
+            when "Zip"
+              #Thread.new { Zipper.zip(File.join(@selected_dir), File.join(ENV["HOME"], "Desktop/#{@zip_file_name}")) }.join
+              Zipper.zip(File.join(@selected_dir), File.join(ENV["HOME"], "Desktop/#{@zip_file_name}"))
+              animate do |i|
+                @zip_progress.fraction = (@dir_contents_size % Zipper.progress ) * 100
+              end
+            when "Unzip"
+              alert("unzip operation selected!")
+            else
+              alert("you must select an operation!")
+            end
           end
         end
       end
